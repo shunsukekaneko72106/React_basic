49faa08f1bbaf47c89ff77e814d705db4fd3be93
shunsuke kaneko <61620829+shunsukekaneko72106@users.noreply.github.com>
2021-06-20 14:14:05 +0900

    変更

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import React from "react";
+import ReactDom from "react-dom";
+
+//JSX記法
+const App = () => {
+  return (
+    // <React.Fragment>これでも良い
+    <>
+      <h1>こんにちわ！</h1>
+      <p>テストです</p>
+    </>
+    // </React.Fragment>
+  );
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
