d55d81bf1aefcef3a15bd6677423e2b842d9f492
shunsuke kaneko <61620829+shunsukekaneko72106@users.noreply.github.com>
2021-06-20 15:19:46 +0900

    設定変更

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+//JSX記法
+const App = () => {
+  return (
+    // <React.Fragment>これでも良い
+    <>
+      <h1>こんにちわ！</h1>
+      <p>テストですね</p>
+    </>
+    // </React.Fragment>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-//JSX記法
-const App = () => {
-  return (
-    // <React.Fragment>これでも良い
-    <>
-      <h1>こんにちわ！</h1>
-      <p>テストです</p>
-    </>
-    // </React.Fragment>
-  );
-};
+import App from "./app";
 
 ReactDom.render(<App />, document.getElementById("root"));
