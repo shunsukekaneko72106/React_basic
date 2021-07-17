717f07a5659dfe6b74635f0f02fde8d4ba998d4c
shunsuke kaneko <61620829+shunsukekaneko72106@users.noreply.github.com>
2021-07-18 08:36:48 +0900

    add code

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,15 @@
 import React from "react";
+import ColorfullMessage from "./components/ColorfullMessage";
 
 //JSX記法
 const App = () => {
   const onClickButton = () => {};
-  const contentStyle = {
-    backgroundColor: "pink"
-  };
+
   return (
     // <React.Fragment>これでも良い
     <>
       <h1 style={{ color: "red" }}>こんにちわ！</h1>
-      <p style={contentStyle}> テストですね</p>
+      <ColorfullMessage />
       <button onClick={onClickButton}>ボタン</button>
     </>
     // </React.Fragment>
--- src/components/ColorfullMessage.jsx
+++ src/components/ColorfullMessage.jsx
@@ -1,1 +1,12 @@
 import React from "react";
+
+const ColorfullMessage = () => {
+  const contentStyle = {
+    backgroundColor: "pink",
+    fontSize: "18px"
+  };
+
+  return <p style={contentStyle}> テストっすね</p>;
+};
+
+export default ColorfullMessage;
