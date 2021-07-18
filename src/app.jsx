7800ce51fb64bfc6feaddeeb73d1178eb016c795
shunsuke kaneko <61620829+shunsukekaneko72106@users.noreply.github.com>
2021-07-18 13:47:44 +0900

    add code

--- src/app.jsx
+++ src/app.jsx
@@ -3,18 +3,25 @@ import ColorfullMessage from "./components/ColorfullMessage";
 
 //JSX記法
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
   return (
     // <React.Fragment>これでも良い
     <>
       <h1 style={{ color: "red" }}>こんにちわ！</h1>
       <ColorfullMessage color="blue" message="お元気ですか？" />
       <button onClick={onClickCountUp}>カウントアップ！</button>
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
-      <p>( ^ω^ )</p>
+      {faceShowFlag && <p>( ^ω^ )</p>}
     </>
     // </React.Fragment>
   );
